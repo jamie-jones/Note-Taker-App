ca519b09349f5556490227ca2b12901dee09de16
Insomnia <jamie.mar.jones14@gmail.com>
2020-09-30 18:18:20 -0400

    added app.post to the code.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
@@ -9,10 +10,11 @@ const PORT = process.env.PORT || 2000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("*", function (req, res) {
@@ -20,9 +22,29 @@ app.get("*", function (req, res) {
 });
 
 // API ROUTES
+app.get("/api/notes", function (req, res) {
+  return res.sendFile(path.join(__dirname, "db/db.json"));
+});
 
 // Starts the server to begin listening
-// =============================================================
+
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+  console.log(`Server Listening on: ${PORT}`);
+});
+
+app.post("/api/notes", function (req, res) {
+  try {
+    notesData = fs.readFileSync("./db/db.json", "utf8");
+    console.log(notesData);
+    notesData = JSON.parse(notData);
+    req.body.id = notesData.length;
+    notesData.push(req.body);
+    notesData = JSON.stringify(notesData);
+    fs.writeFile("./db/db.json", notesData, "utf8", function (err) {
+      if (err) throw err;
+    });
+    res.json(JSON.parse(notesData));
+  } catch (err) {
+    throw err;
+  }
 });
