72730ffb89b0373a02bae52f025645115da3cac0
Insomnia <jamie.mar.jones14@gmail.com>
2020-10-01 17:52:25 -0400

    I don't know what I'm doing at all.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { isBuffer } = require("util");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -28,62 +28,28 @@ app.get("/notes", function (req, res) {
 //   return res.sendFile(path.join(__dirname, "db/db.json"));
 // });
 
-app.get("/api/notes", (req, res) => {
-  fs.readFile("db/db.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({
-        error: true,
-        data: null,
-        messages: "Unable to retrieve note.",
-      });
-    }
-    res.json({
-      error: false,
-      data: JSON.parse(data),
-      messages: "Successfully retrieved note.",
-    });
-  });
-});
+// app.get("/api/notes", (req, res) => {
+//   fs.readFile("db/db.json", "utf-8", (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       return res.status(500).json({
+//         error: true,
+//         data: null,
+//         messages: "Unable to retrieve note.",
+//       });
+//     }
+//     res.json({
+//       error: false,
+//       data: JSON.parse(data),
+//       messages: "Successfully retrieved note.",
+//     });
+//   });
+// });
 
 // Starts the server to begin listening
 
 app.post("api/notes", (req, res) => {
   console.log(req.body);
-  if (!req.body.name || !req.body.grade) {
-    return res.status(400).json({
-      error: true,
-      data: null,
-      message: "Invalid note.",
-    });
-  }
-  fs.readFile("db/db.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({
-        error: true,
-        data: null,
-        message: "Unable to retrieve note.",
-      });
-    }
-    const updatedData = JSON.parse(data);
-    updatedData.push(req.body);
-    fs.writeFile("db/db.json", JSON.stringify(updatedData), (err) => {
-      if (err) {
-        console.lof(err);
-        return res.status(500).json({
-          error: true,
-          data: null,
-          message: "Unable to save note.",
-        });
-      }
-      res.json({
-        error: false,
-        data: updatedData,
-        message: "Successfully added a new note.",
-      });
-    });
-  });
 });
 
 // app.post("/api/notes", function (req, res) {
@@ -105,7 +71,7 @@ app.post("api/notes", (req, res) => {
 // });
 
 app.listen(PORT, function () {
-  console.log(`Server Listening on: ${PORT}`);
+  console.log(`Server Listening on http://localhost:${PORT}`);
 });
 
 // app.delete("/api/notes", function)
